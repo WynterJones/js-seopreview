a7ee29fa989711ff61049f8e8b514d80ad132c50
monetizedesign <wynter@monetizedesign.com>
2018-01-05 06:09:34 -0500

    jslint plugin + css comments

--- js/jquery-seopreview.js
+++ js/jquery-seopreview.js
@@ -9,79 +9,144 @@
  *
  * Licensed under the MIT license:
  * https://opensource.org/licenses/MIT
+ *
 */
 
-(function($) {
+/*global jQuery */ // jslint.com
+/* Property Directives
+    attr, author, base_domain, date, desc, desc_limit, each, extend, facebook,
+    featured_image, find, fn, getDate, getFullYear, getMonth, google, html,
+    label, label_text, length, metadata, nodeName, on, preventDefault,
+    seoPreview, styled, substring, tabs, text, title, title_limit, type, url,
+    url_limit, use_slug, val
+*/
 
-    $.fn.seoPreview = function(options) {
+(function ($) {
+    "use strict";
+
+    $.fn.seoPreview = function (options) {
 
         // Options
         var settings = $.extend({
             metadata: {
-                title: 'Live SEO Preview for Your Web Apps',
-                url: 'http://example.com/custom-url',
-                desc: 'The jQuery SEO Preview Plugin is a simple plugin that allows you to preview meta tag data in a styled Google, Facebook and other social link previews. You can bind the preview to inputs to get live preview while editing perfect for blog and CMS applications.'
+                base_domain: "",
+                use_slug: false,
+                title: "MonetizeDesign - jQuery Plugin - SEO Live Preview",
+                url: "https://monetizedesign.github.io/js-seopreview/",
+                desc: "A simple jQuery plugin that allows you to preview meta tag data in a styled Google and Facebook link preview."
             },
-            gg_title_limit: 60,
-            gg_url_limit: 72,
-            gg_desc_limit: 300
+            google: {
+                label: true,
+                label_text: "Google-view",
+                date: true,
+                title_limit: 60,
+                url_limit: 72,
+                desc_limit: 300
+            },
+            facebook: {
+                label: true,
+                label_text: "Facebook-view",
+                author: "",
+                featured_image: ""
+            },
+            styled: true,
+            tabs: true
         }, options);
 
-        // Check If Is Input Editable
+        // Turncate Meta Data
+        function truncate(str, limit) {
+            if (str.length < limit) {
+                var truncated_text = str.substring(0, limit);
+                truncated_text = truncated_text + " ...";
+            }
+            return str;
+        }
+
+        // Bind Each <input> <textarea> on Keyup
+        $.each(settings.metadata, function (index, value) {
+            if (value[0]) {
+                if (value[0].type === "text" || value[0].type === "textarea") {
+                    var character_limit = "";
+                    if (index === "title") {
+                        character_limit = settings.google.title_limit;
+                    } else if (index === "url") {
+                        character_limit = settings.google.url_limit;
+                    } else if (index === "desc") {
+                        character_limit = settings.google.desc_limit;
+                    }
+                    $(document).find(value).attr("data-md-limit", character_limit);
+                    $(document).on("keyup", value, function (event) {
+                        event.preventDefault();
+                        character_limit = parseInt($(document).find(value).attr("data-md-limit"));
+                        $(document).find("span#md-google-" + index).text(truncate(value.val(), character_limit));
+                    });
+                }
+            }
+        });
+
+        // Check if <input type="text"> or <textarea>
         function isInput(option) {
-            if (option[0].nodeName == "INPUT" && option[0].type == "text" || option[0].nodeName == "TEXTAREA") {
+            if ((option[0].nodeName === "INPUT" && option[0].type === "text") || option[0].nodeName === "TEXTAREA") {
                 return true;
             }
         }
 
-        // Truncate Character Limit - from: https://stackoverflow.com/questions/1199352/smart-way-to-shorten-long-strings-with-javascript
-        function truncate(str, limit) {
-            return (str.length < limit) ? str : str.substring(0, limit) + ' ...';
-        }
-
-        // Bind to Keyup on Inputs
-        $.each(settings.metadata, function(index, value) {
-            if (value[0].type == "text" || value[0].type == "textarea") {
-                if (index == 'title') {
-                    character_limit = settings.gg_title_limit;
-                }
-                else if (index == 'url') {
-                    character_limit = settings.gg_url_limit;
-                }
-                else if (index == 'desc') {
-                    character_limit = settings.gg_desc_limit;
-                }
-                $(document).find(value).attr('data-md-limit', character_limit);
-                $(document).on('keyup', value, function(event) {
-                    event.preventDefault();
-                    character_limit = parseInt($(document).find(value).attr('data-md-limit'));
-                    $(document).find('span#md-gg-' + index).text(truncate(value.val(), character_limit));
-                });
-            }
-        });
-
-        // Set Defaults
+        // Set Meta Title Default
+        var metaTitle = settings.metadata.title;
         if (isInput(settings.metadata.title)) {
-            metaTitle = settings.metadata.title.val()
-        } else {
-            metaTitle = settings.metadata.title
+            metaTitle = settings.metadata.title.val();
         }
 
+        // Set Meta Meta URL Default
+        var metaURL = settings.metadata.url;
         if (isInput(settings.metadata.url)) {
-            metaURL = settings.metadata.url.val()
-        } else {
-            metaURL = settings.metadata.url
+            metaURL = settings.metadata.url.val();
         }
 
+        // Set Meta Description Default
+        var metaDesc = settings.metadata.title;
         if (isInput(settings.metadata.desc)) {
-            metaDesc = settings.metadata.desc.val()
-        } else {
-            metaDesc = settings.metadata.desc
+            metaDesc = settings.metadata.desc.val();
         }
 
+        // Option: Google Date
+        var google_date = "";
+        if (settings.google.date === true) {
+            var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+            var d = new Date();
+            var date = d.getDate();
+            var month = d.getMonth();
+            var year = d.getFullYear();
+            var today = months[month] + " " + date + ", " + year;
+            google_date = "<span id='md-js-seo-preview__google-date'>" + today + " - </span>";
+        }
 
-        // Apply options
-        return this.html('<div id="md-seo-previewer" class="mdcleanslate-seopreview"><div id="md-seo-previewer-gg-inner"><span id="md-gg-title">' + truncate(metaTitle, settings.gg_title_limit) + '</span><span id="md-gg-url">' + truncate(metaURL, settings.gg_url_limit) + '</span><span id="md-gg-desc">' + truncate(metaDesc, settings.gg_desc_limit) + '</span><small id="md-gg-preview">Google Preview</small></div></div>');
+        // Show Styled or Unstyled
+        var theme_class = "";
+        if (settings.styled === true) {
+            theme_class = "md-js-seo-preview__theme";
+        }
+        // Option: Google Label
+        var google_label = "";
+        if (settings.google.label === true) {
+            google_label = "<small id='md-js-seo-preview__google-label'>" + settings.google.label_text + "</small>";
+        }
+
+        // Make: Output HTML
+        var preview_box = "";
+        preview_box += "<div id='md-js-seo-preview' class='md-js-cleanslate " + theme_class + "''>";
+        preview_box += "<div id='md-js-seo-preview__google-inner'>";
+        preview_box += "<span id='md-js-seo-preview__google-title'>" + truncate(metaTitle, settings.google.title_limit) + "</span>";
+        preview_box += "<span id='md-js-seo-preview__google-url'>" + truncate(metaURL, settings.google.url_limit) + "</span>";
+        preview_box += "<span id='md-js-seo-preview__google-desc'>" + google_date + truncate(metaDesc, settings.google.desc_limit) + "</span>";
+
+        if (settings.google.label === true) {
+            preview_box += google_label;
+        }
+        preview_box += "</div></div>";
+
+        // Add Preview to Page
+        return this.html(preview_box);
 
     };
 
