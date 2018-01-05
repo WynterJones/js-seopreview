d861747bea9fd1006b23107c4f85b9c2b02ac2ed
monetizedesign <wynter@monetizedesign.com>
2018-01-05 06:37:53 -0500

    minor changes

--- js/jquery-seopreview.js
+++ js/jquery-seopreview.js
@@ -32,9 +32,9 @@
             metadata: {
                 base_domain: "",
                 use_slug: false,
-                title: "MonetizeDesign - jQuery Plugin - SEO Live Preview",
+                title: "MonetizeDesign - jQuery Plugin - jQuery SEO Live Preview Plugin",
                 url: "https://monetizedesign.github.io/js-seopreview/",
-                desc: "A simple jQuery plugin that allows you to preview meta tag data in a styled Google and Facebook link preview."
+                desc: "The jQuery SEO Preview Plugin is a simple plugin that allows you to prdfeview meta tag data in a styled Google, Facebook and other social link previews. You can bind the preview to inputs to get live preview while editing perfect for blog and CMS applications."
             },
             google: {
                 label: true,
@@ -54,13 +54,33 @@
             tabs: true
         }, options);
 
-        // Turncate Meta Data
+        // CSS Class
+        var main_class = "md-js-seo-preview";
+
+        // Function: Turncate Meta Data
         function truncate(str, limit) {
             if (str.length < limit) {
                 var truncated_text = str.substring(0, limit);
                 truncated_text = truncated_text + " ...";
             }
             return str;
+        }
+
+        // Function: Check if <input type="text"> or <textarea>
+        function isInput(option) {
+            if ((option[0].nodeName === "INPUT" && option[0].type === "text") || option[0].nodeName === "TEXTAREA") {
+                return true;
+            }
+        }
+
+        // Function: Get todays date
+        function todaysDate() {
+            var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+            var d = new Date();
+            var date = d.getDate();
+            var month = d.getMonth();
+            var year = d.getFullYear();
+            return months[month] + " " + date + ", " + year;
         }
 
         // Bind Each <input> <textarea> on Keyup
@@ -85,13 +105,6 @@
             }
         });
 
-        // Check if <input type="text"> or <textarea>
-        function isInput(option) {
-            if ((option[0].nodeName === "INPUT" && option[0].type === "text") || option[0].nodeName === "TEXTAREA") {
-                return true;
-            }
-        }
-
         // Set Meta Title Default
         var metaTitle = settings.metadata.title;
         if (isInput(settings.metadata.title)) {
@@ -110,29 +123,20 @@
             metaDesc = settings.metadata.desc.val();
         }
 
-        // CSS Class
-        var main_class = "md-js-seo-preview";
-
         // Option: Google Date
-        var google_date = "";
+        var google_date;
         if (settings.google.date === true) {
-            var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-            var d = new Date();
-            var date = d.getDate();
-            var month = d.getMonth();
-            var year = d.getFullYear();
-            var today = months[month] + " " + date + ", " + year;
-            google_date = "<span id='" + main_class + "__google-date'>" + today + " - </span>";
+            google_date = "<span id='" + main_class + "__google-date'>" + todaysDate() + " - </span>";
         }
 
         // Show Styled or Unstyled
-        var theme_class = "";
+        var theme_class;
         if (settings.styled === true) {
             theme_class = "" + main_class + "__theme";
         }
         // Option: Google Label
-        var google_label = "";
-        if (settings.google.label === true) {
+        var google_label;
+        if (settings.google.label === true && theme_class !== "") {
             google_label = "<small id='" + main_class + "__google-label'>" + settings.google.label_text + "</small>";
         }
 
